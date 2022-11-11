f75f7b521902ab2e0419d6337b965a24ba8f5c61
Sdev0212 <seandevine0212@gmail.com>
2022-11-11 17:14:37 +0000

    Passed tests for exercises 1 2 and 3

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,11 @@
-const repeatString = function() {
-
+const repeatString = function(Str, num) {
+    var repeat = "";
+    if(num<0) {
+        repeat = "ERROR";
+    } else for (let i = 0; i < num; i++){
+        repeat = Str + repeat;
+    }
+    return repeat;
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    array = string.split("");
+    let newString = "";
+    let length = array.length - 1;
+    for (let i=0; i <= length; i++) {
+        newString = newString + array[length - i];
+    }
+    return newString;
 };
 
 // Do not edit below this line

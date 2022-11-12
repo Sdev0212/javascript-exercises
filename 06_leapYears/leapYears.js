fbef74b0f39d6837a4de8fb66108816735d4429c
Sdev0212 <seandevine0212@gmail.com>
2022-11-12 12:33:39 +0000

    Complete leapYears.js

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,12 @@
-const leapYears = function() {
-
+const leapYears = function(year) {
+    let remainder4 = year % 4;
+    let remainder100 = year % 100;
+    let remainder400 = year % 400;
+    if (remainder4 != 0){
+        return false;
+    } else if (remainder4 == 0 && remainder100 == 0 && remainder400 != 0) {
+        return false;
+    } else return true;
 };
 
 // Do not edit below this line

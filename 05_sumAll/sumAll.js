71f1bee008b70ac8600c675f5233e811a8a4e603
Sdev0212 <seandevine0212@gmail.com>
2022-11-12 12:21:31 +0000

    complete sumAll.js

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(num1, num2) {
+    let sum = 0;
+    if (typeof(num1)!="number"||typeof(num2)!="number"||num1<0||num2<0){
+        return "ERROR";
+    } else if(num1 < num2){
+        for (num1; num1<=num2; num1++){
+            sum = sum + num1;
+        }
+    } else if(num2 < num1) {
+        for(num2; num2<=num1; num2++){
+            sum = sum +num2;
+        }
+    }
+    return sum;
 };
 
 // Do not edit below this line

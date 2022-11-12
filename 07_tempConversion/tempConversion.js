d8d9e6ff1e35737fa6191c223fc16072ca6b3796
Sdev0212 <seandevine0212@gmail.com>
2022-11-12 12:45:30 +0000

    complete tempConversion.js

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,9 +1,13 @@
-const ftoc = function() {
-
+const ftoc = function(far) {
+  let cel = (far - 32) * (5/9);
+  cel = Math.round(cel*10)/10;
+  return cel;
 };
 
-const ctof = function() {
-
+const ctof = function(cel) {
+  let far = cel * (9/5) + 32;
+  far = Math.round(far*10)/10;
+  return far;
 };
 
 // Do not edit below this line
